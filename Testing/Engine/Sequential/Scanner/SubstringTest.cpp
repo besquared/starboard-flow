f9d442d1b73d52beaaaff85799fd347a3a89287c
Josh Ferguson <Josh@Besquared.local>
2010-02-01 13:17:00 -0800

    test for sequences where the scanner must do shifts

--- Testing/Engine/Sequential/Scanner/SubstringTest.cpp
+++ Testing/Engine/Sequential/Scanner/SubstringTest.cpp
@@ -69,4 +69,55 @@ namespace {
     ASSERT_EQ(1, matches.matches[0].size());
     EXPECT_EQ(0, matches.matches[0][0]);
 	}
+  
+  TEST_F(SubstringTest, MatchesShiftingSequences) {
+    Conditions conditions1;
+    Conditions conditions2;
+    Conditions conditions3;
+    Conditions conditions4;
+    conditions1.Eq("action", "in");
+    conditions2.Eq("action", "out");
+    conditions3.Eq("action", "in");
+    conditions4.Eq("action", "out");
+    
+    Sequential::Pattern pattern;
+		pattern.push_back("station1", "X", "x1", conditions1);
+    pattern.push_back("station2", "Y", "y1", conditions2);
+    pattern.push_back("station3", "Y", "y2", conditions3);
+    pattern.push_back("station4", "X", "x2", conditions4);
+    
+    vector<string> values;
+    values.push_back("S1");
+    values.push_back("Fall");
+    Engine::Group s1_fall(values);
+    
+    s1_fall.records.push_back(90);
+    s1_fall.records.push_back(100);
+    s1_fall.records.push_back(476);
+    s1_fall.records.push_back(728);
+    s1_fall.records.push_back(937);
+    
+    s1_fall.dimensions["station"].push_back("montgomery");
+    s1_fall.dimensions["station"].push_back("montgomery");
+    s1_fall.dimensions["station"].push_back("16th street");
+    s1_fall.dimensions["station"].push_back("16th street");
+    s1_fall.dimensions["station"].push_back("montgomery");
+    
+    s1_fall.dimensions["action"].push_back("in");
+    s1_fall.dimensions["action"].push_back("in");
+    s1_fall.dimensions["action"].push_back("out");
+    s1_fall.dimensions["action"].push_back("in");
+    s1_fall.dimensions["action"].push_back("out");
+    
+    Engine::Groups sequences;
+		sequences.push_back(s1_fall);
+    
+    Sequential::Scanner::Matches matches;
+    Sequential::Scanner::Substring scanner;
+    scanner.execute(pattern, sequences, matches);
+    
+    ASSERT_EQ(1, matches.matches.size());
+    ASSERT_EQ(1, matches.matches[0].size());
+    EXPECT_EQ(1, matches.matches[0][0]);
+	}  
 }
